ea6f56e6ff32a01a656e46aee0ff81f540a802e2
Duncan Morais <duncanmorais@gmail.com>
2024-02-24 22:20:57 -0700

    reorganize event with class

---
+++ public/scripts/_Event.mjs
@@ -0,0 +1,40 @@
+import { GoogleMap, Bounds } from "./_maps.mjs"
+
+export default class Event {
+    constructor(id) {
+        this.id = id;
+    }
+
+    async load() {
+        const dataString = localStorage.getItem(this.id);
+        if (!dataString) return null;
+
+        const data = JSON.parse(dataString);
+        Object.assign(this, data);
+        return this;
+    }
+
+    render(container, onclick, btnMsg) {
+        const eventWrapper = document.createElement('div');
+        eventWrapper.className = 'event';
+        eventWrapper.addEventListener('click', onclick)
+        eventWrapper.innerHTML = `
+            <div class="map">
+                <div class="placeholder shimmer"></div>
+            </div>
+            <div class="body">
+                <div class="header">
+                    <h3>${this.name}</h3>
+                    <p class="countdown"></p>
+                </div>
+                <p>${this.description}</p>
+            </div>
+            <button>${btnMsg}</button>
+        `
+        container.appendChild(eventWrapper);
+    
+        const mapWrapper = eventWrapper.querySelector('.map');
+        const bounds = new Bounds(this.bounds);
+        new GoogleMap(mapWrapper, bounds.center(), bounds);
+    }
+}
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,23 @@
 import "./_auth.mjs";
+import Event from "./_Event.mjs";
 import { restartJavascriptDeliverable } from "./_helpers.mjs";
 
 document.getElementById('restart').addEventListener('click', restartJavascriptDeliverable)
+
+
+async function initialize() {
+    const eventIDsString = localStorage.getItem(`events`) ?? "[]";
+    const eventIDs = JSON.parse(eventIDsString);
+
+    eventIDs.forEach(async eventID => {
+        const event = new Event(eventID);
+        await event.load();
+        event.render(
+            document.getElementById('live-events'),
+            () => location.href = `event.html?e=${eventID}`,
+            'View'
+        )
+    });
+}
+
+initialize()
